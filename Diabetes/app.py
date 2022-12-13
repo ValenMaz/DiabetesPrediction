aa70734235d7ae68148c888c96332c593deccc0a
ValenMaz <valentinamazxd@gmail.com>
2022-12-13 15:08:37 -0500

    Cambios en tablas y función diabetes

--- Diabetes/app.py
+++ Diabetes/app.py
@@ -16,6 +16,7 @@ from sklearn.model_selection import train_test_split
 from flask_bcrypt import Bcrypt
 from flask_bcrypt import generate_password_hash
 from flask_bcrypt import check_password_hash
+from flask_migrate import Migrate
 filename = 'diabetes-prediction-rfc-model.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
@@ -27,22 +28,33 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhos
 #postgresql://postgres:postgres@localhost/database-sqlite:///database.db
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 bcrypt = Bcrypt(app)
 
+
+############################################################################################################
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+    user_predictions = db.relationship('UserPredictions', backref='user')
+
+class UserPredictions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date = db.Column(db.DateTime)
+    result = db.Column(db.String(25))
+############################################################################################################
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
+############################################################################################################
 #Forms.py
 
 class LoginForm(FlaskForm):
@@ -56,6 +68,7 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
 
+############################################################################################################
 #views.py
 
 @app.route('/')
@@ -116,82 +129,12 @@ def dashboard():
 def disindex():
     return render_template("disindex.html")
 
-
-@app.route("/cancer")
-@login_required
-def cancer():
-    return render_template("cancer.html")
-
+############################################################################################################
 
 @app.route("/diabetes")
 @login_required
 def diabetes():
     return render_template("diabetes.html")
-
-
-@app.route("/heart")
-@login_required
-def heart():
-    return render_template("heart.html")
-
-
-@app.route("/kidney")
-@login_required
-def kidney():
-    return render_template("kidney.html")
-
-
-def ValuePredictor(to_predict_list, size):
-    to_predict = np.array(to_predict_list).reshape(1, size)
-    if size == 7:
-        loaded_model = joblib.load('kidney_model.pkl')
-        result = loaded_model.predict(to_predict)
-    return result[0]
-
-
-@app.route("/predictkidney",  methods=['GET', 'POST'])
-def predictkidney():
-    if request.method == "POST":
-        to_predict_list = request.form.to_dict()
-        to_predict_list = list(to_predict_list.values())
-        to_predict_list = list(map(float, to_predict_list))
-        if len(to_predict_list) == 7:
-            result = ValuePredictor(to_predict_list, 7)
-    if(int(result) == 1):
-        prediction = "Patient has a high risk of Kidney Disease, please consult your doctor immediately"
-    else:
-        prediction = "Patient has a low risk of Kidney Disease"
-    return render_template("kidney_result.html", prediction_text=prediction)
-
-
-@app.route("/liver")
-@login_required
-def liver():
-    return render_template("liver.html")
-
-
-def ValuePred(to_predict_list, size):
-    to_predict = np.array(to_predict_list).reshape(1,size)
-    if(size==7):
-        loaded_model = joblib.load('liver_model.pkl')
-        result = loaded_model.predict(to_predict)
-    return result[0]
-
-
-@app.route('/predictliver', methods=["POST"])
-def predictliver():
-    if request.method == "POST":
-        to_predict_list = request.form.to_dict()
-        to_predict_list = list(to_predict_list.values())
-        to_predict_list = list(map(float, to_predict_list))
-        if len(to_predict_list) == 7:
-            result = ValuePred(to_predict_list, 7)
-
-    if int(result) == 1:
-        prediction = "Patient has a high risk of Liver Disease, please consult your doctor immediately"
-    else:
-        prediction = "Patient has a low risk of Kidney Disease"
-    return render_template("liver_result.html", prediction_text=prediction)
 
 
 @app.route('/logout')
@@ -200,21 +143,6 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    input_features = [int(x) for x in request.form.values()]
-    features_value = [np.array(input_features)]
-    features_name = ['clump_thickness', 'uniform_cell_size', 'uniform_cell_shape', 'marginal_adhesion',
-                     'single_epithelial_size', 'bare_nuclei', 'bland_chromatin', 'normal_nucleoli', 'mitoses']
-    df = pd.DataFrame(features_value, columns=features_name)
-    output = model.predict(df)
-    if output == 4:
-        res_val = "a high risk of Breast Cancer"
-    else:
-        res_val = "a low risk of Breast Cancer"
-
-    return render_template('cancer_result.html', prediction_text='Patient has {}'.format(res_val))
 
 
 ##################################################################################
@@ -255,6 +183,7 @@ pickle.dump(classifier, open(filename, 'wb'))
 #####################################################################
 
 
+
 @app.route('/predictt', methods=['POST'])
 def predictt():
     if request.method == 'POST':
@@ -270,34 +199,22 @@ def predictt():
         data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
         my_prediction = classifier.predict(data)
 
-        return render_template('diab_result.html', prediction=my_prediction)
-
-
-############################################################################################################
-
-@app.route('/predictheart', methods=['POST'])
-def predictheart():
-    input_features = [float(x) for x in request.form.values()]
-    features_value = [np.array(input_features)]
-
-    features_name = ["age", "trestbps", "chol", "thalach", "oldpeak", "sex_0",
-                     "  sex_1", "cp_0", "cp_1", "cp_2", "cp_3", "  fbs_0",
-                     "restecg_0", "restecg_1", "restecg_2", "exang_0", "exang_1",
-                     "slope_0", "slope_1", "slope_2", "ca_0", "ca_1", "ca_2", "thal_1",
-                     "thal_2", "thal_3"]
-
-    df = pd.DataFrame(features_value, columns=features_name)
-    output = model1.predict(df)
-
-    if output == 1:
-        res_val = "a high risk of Heart Disease"
+    if my_prediction == 1:
+        res_val = "a high risk of Diabetes"
     else:
-        res_val = "a low risk of Heart Disease"
+        res_val = "a low risk of Diabetes"
 
-    return render_template('heart_result.html', prediction_text='Patient has {}'.format(res_val))
+
+       
+    return render_template('diab_result.html',  prediction_text='Patient has {}'.format(res_val))
 
 
 ############################################################################################################
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
